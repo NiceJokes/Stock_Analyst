a1d48a9db8822ceeae5e6cf553b7ae6288cb0804
Lukasz Terpilowski <terpilowski.l@gmail.com>
2020-04-19 11:55:13 +0200

    update

--- stocks_proj/importdata.py
+++ stocks_proj/importdata.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 import csv
-from .models import API_keys, Items_table, Indicators,Countries
+from .models import API_keys, Items_table, Indicators
 
 
